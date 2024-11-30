abe7c040d7f4d970ef0330d014d692befd785df0
samych02 <cherif.samy1000@gmail.com>
2024-11-30 22:11:07 +0100

    commit

--- src/Graphic.h
+++ src/Graphic.h
@@ -17,27 +17,27 @@ public:
   explicit Graphic(const size_t width, const size_t height) : width(width), height(height), size(width * height),
                                                               buffer(new T[size])
   {
-    memset(this->buffer, 0, sizeof(T) * size);
+    memset(buffer, 0, sizeof(T) * size);
   }
 
   ~Graphic()
   {
-    delete[] this->buffer;
+    delete[] buffer;
   }
 
   void Clear() const
   {
-    memset(this->buffer, 0, sizeof(T) * size);
+    memset(buffer, 0, sizeof(T) * size);
   }
 
   [[nodiscard]] size_t GetWidth() const
   {
-    return this->width;
+    return width;
   }
 
   [[nodiscard]] size_t GetHeight() const
   {
-    return this->height;
+    return height;
   }
 
   // if I understood correctly, the drawing mechanism is as follows:
@@ -46,29 +46,36 @@ public:
   // each byte represent a row of 8 pixels (bits) which will be put in video buffer
   // the next byte from memory will be put in the next row.
   // then we set Vf to 1 for collision detection
-  void drawSprite(size_t Vx, size_t Vy, std::vector<T>& sprite, Register<uint8_t> VF)
+  // edit: we are using uint32 to make compatible with SDL_UpdateTexture so 0x0 for 0 and 0xFFFFFFFF for 1
+  void drawSprite(const size_t Vx, const size_t Vy, const std::vector<uint8_t>& sprite, Register<uint8_t>& VF)
   {
-    const size_t posX = Vx % this->width;
-    const size_t posY = Vy % this->height;
+    const size_t posX = Vx % width;
+    const size_t posY = Vy % height;
 
     for (int spriteRowIndex = 0; spriteRowIndex < sprite.size(); ++spriteRowIndex)
     {
-      T& spriteRow = sprite[spriteRowIndex];
+      const uint8_t spriteRow = sprite[spriteRowIndex];
       for (int pixelIndex = 0; pixelIndex < 8; ++pixelIndex)
       {
         // we get the pixel from buffer at coordinates (posX, posY)
         // that formule convert coordinates (x,y) to an array index
         // the % to wrap to the other side
-        T& bufferPixel = buffer[(((posY + spriteRowIndex) * this->width) % this->height) + ((posX + pixelIndex) % this->width)];
-        T& spritePixel = sprite[(spriteRow >> (7 - spriteRowIndex)) & 0x1u];
-
+        T& bufferPixel = buffer[(((posY + spriteRowIndex) * width) ) + (posX + pixelIndex)];
         // for optimization purposes, we are going to skip the off pixel in the sprite
-        if (spritePixel == 0x1u)
+        if (const uint8_t& spritePixel = (spriteRow >> (7 - pixelIndex)) & 0x1u; spritePixel == 0x1u)
         {
-        if (bufferPixel == 0x1u) VF.setAddress(1);
-          bufferPixel ^= 0x1u;
+          if (bufferPixel == 0xFFFFFFFFu)
+          {
+            VF.setAddress(1);
+          }
+          bufferPixel ^= 0xFFFFFFFFu;
         }
       }
     }
+  }
+
+  T* getBuffer() const
+  {
+    return buffer;
   }
 };
