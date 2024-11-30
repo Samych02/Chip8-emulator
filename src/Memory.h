0b8415b6c3ef9245ba450df4de1a87ef0541c4ef
samych02 <cherif.samy1000@gmail.com>
2024-11-30 22:11:49 +0100

    commit

--- src/Memory.h
+++ src/Memory.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 
 template <typename T>
@@ -12,7 +11,7 @@ class Memory
 public:
   explicit Memory(const size_t size) : size(size), memory(new T[size])
   {
-    memset(this->memory, 0, sizeof(T) * size);
+    memset(memory, 0, sizeof(T) * size);
   }
 
   ~Memory()
@@ -20,49 +19,49 @@ public:
     delete[] memory;
   }
 
-  void writeByte(size_t address, uint8_t data) const
+  void writeByte(size_t address, const uint8_t data) const
   {
-    if (address >= this->size) throw std::out_of_range("Address out of range");
+    if (address >= size) throw std::out_of_range("Address out of range");
     memory[address] = data;
   }
 
   void writeWord(size_t address, const uint16_t data) const
   {
-    if (address + 1 >= this->size) throw std::out_of_range("Address out of range");
-    this->memory[address] = data & 0xff;
-    this->memory[address + 1] = data >> 8;
+    if (address + 1 >= size) throw std::out_of_range("Address out of range");
+    memory[address] = static_cast<uint8_t>(data & 0xff);
+    memory[address + 1] = static_cast<uint8_t>(data >> 8);
   }
 
-  void writeBytes(size_t address, const std::vector<T>& data) const
+  void writeBytes(size_t address, const std::vector<uint8_t>& data) const
   {
-    if (address + data.size() >= this->size) throw std::out_of_range("Address out of range");
+    if (address + data.size() >= size) throw std::out_of_range("Address out of range");
 
-    for (const T byte : data)
+    for (const uint8_t byte : data)
     {
-      this->memory[address++] = byte;
+      memory[address++] = byte;
     }
 
   }
   [[nodiscard]] uint8_t readByte(const size_t address) const
   {
-    if (address >= this->size) throw std::out_of_range("Address out of range");
-    return this->memory[address];
+    if (address >= size) throw std::out_of_range("Address out of range");
+    return memory[address];
   }
 
   [[nodiscard]] uint16_t readWord(const size_t address) const
   {
-    if (address + 1 >= this->size) throw std::out_of_range("Address out of range");
-    return (static_cast<uint16_t>(this->memory[address]) << 8) | this->memory[address + 1];
+    if (address + 1 >= size) throw std::out_of_range("Address out of range");
+    return (static_cast<uint16_t>(memory[address]) << 8) | memory[address + 1];
   }
 
   std::vector<T> readBytes(const size_t startingAddress, const size_t length) const
   {
-    if (startingAddress + length >= this->size) throw std::out_of_range("Address out of range");
+    if (startingAddress + length >= size) throw std::out_of_range("Address out of range");
     std::vector<T> result;
     result.reserve(length);
     for (size_t i = 0; i < length; i++)
     {
-      result.push_back(this->memory[startingAddress + i]);
+      result.push_back(memory[startingAddress + i]);
     }
     return result;
   }};
